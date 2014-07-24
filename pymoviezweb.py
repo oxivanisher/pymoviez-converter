e643ca24571750496b1cafbda557c9e190156e0b
Marc Urben <aegnor@mittelerde.ch>
2014-07-24 14:36:44 +0200

    debugging current_app.name

--- pymoviezweb.py
+++ pymoviezweb.py
@@ -18,7 +18,7 @@ import logging
 logging.basicConfig(filename='/tmp/pymoviezweb.log', format='%(asctime)s %(levelname)s:%(message)s', datefmt='%Y-%d-%m %H:%M:%S', level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, make_response, send_from_directory, current_app, app_context
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, make_response, send_from_directory, current_app
 
 try:
     from imdb import IMDb, IMDbError
@@ -35,7 +35,7 @@ app.config['scriptPath'] = os.path.dirname(os.path.realpath(__file__))
 
 if not app.debug:
     from logging.handlers import SMTPHandler
-    mail_handler = SMTPHandler(app.config['EMAILSERVER'], app.config['EMAILFROM'], ADMINS, __name__ + ' failed!')
+    mail_handler = SMTPHandler(app.config['EMAILSERVER'], app.config['EMAILFROM'], ADMINS, current_app.name + ' failed!')
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
 
